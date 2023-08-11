be921d1b009074e77a8939f510b9a71fd27d1f21
Dzmitry Zhuk <dzmitry.zhuk@intexsoft.by>
2023-08-11 15:53:30 +0300

    upf

--- src/ast.h
+++ src/ast.h
@@ -7,6 +7,7 @@
 
 // AST expression
 class ASTExpr {
+    // TODO: add type of expression like number, string, etc.
 public:
     virtual ~ASTExpr() = default;
 };
@@ -25,7 +26,7 @@ public:
     ASTExprVariable(const std::string& name) : m_name(name) {}
 };
 
-/// Expression class for a binary operator.
+// Expression class for a binary operator.
 class ASTExprBinary : public ASTExpr {
   char m_operator;
   std::unique_ptr<ASTExpr> m_leftOperand, m_rightOperand;
@@ -35,7 +36,7 @@ public:
     : m_operator(operator_), m_leftOperand(std::move(left)), m_rightOperand(std::move(right)) {}
 };
 
-/// Expression class for function calls
+// Expression class for function calls
 class ASTExprCall : public ASTExpr {
   std::string m_callee;
   std::vector<std::unique_ptr<ASTExpr>> m_args;
@@ -43,6 +44,28 @@ class ASTExprCall : public ASTExpr {
 public:
   ASTExprCall(const std::string &callee, std::vector<std::unique_ptr<ASTExpr>> args)
     : m_callee(callee), m_args(std::move(args)) {}
+};
+
+// This class represents the "prototype" for a function, which captures its name, and its argument names
+class ASTPrototype {
+  std::string m_name;
+  std::vector<std::string> m_args;
+
+public:
+  ASTPrototype(const std::string &name, std::vector<std::string> args)
+    : m_name(name), m_args(std::move(args)) {}
+
+  const std::string &getName() const { return m_name; }
+};
+
+// This class represents a function definition itself
+class ASTFunction {
+  std::unique_ptr<ASTPrototype> m_proto;
+  std::unique_ptr<ASTExpr> m_body;
+
+public:
+  ASTFunction(std::unique_ptr<ASTPrototype> proto, std::unique_ptr<ASTExpr> body)
+    : m_proto(std::move(proto)), m_body(std::move(body)) {}
 };
 
 #endif
