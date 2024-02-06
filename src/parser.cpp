f0688c9a8c9a88f42a93adfae5d40b9f8e7c593f
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-06 16:43:02 +0300

    upd parser: it has lexer  as a part now

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,1 +1,15 @@
 #include "parser.h"
+
+#include <fstream>
+
+Parser::Parser(const std::filesystem::path &path)
+    : file_(path)
+{
+  auto fileSize = std::filesystem::file_size(path);
+  std::string fileContent;
+  fileContent.resize(fileSize);
+  std::ifstream stream{path};
+  stream.read(fileContent.data(), fileSize);
+  stream.close();
+  lexer_ = std::make_unique<Lexer>(fileContent);
+}
--- src/parser.h
+++ src/parser.h
@@ -1,25 +1,33 @@
 #pragma once
 
+#include "lexer.h"
+
 #include <map>
 #include <string>
+#include <filesystem>
 
 class Parser
 {
-    int m_curTok;
-    // BinopPrecedence - This holds the precedence for each binary operator that is defined
-    static std::map<char, int> m_binopPrecedence;
-public:
-    //int getNextToken() { return m_curTok = gettok(); }
+    public:
+        Parser(const std::filesystem::path &path);
+        virtual ~Parser() = default;
+        std::vector<Lexer::Token> tokenize(const std::string & source);
+        // /// GetTokPrecedence - Get the precedence of the pending binary operator token.
+        // static int GetTokPrecedence() {
+        // if (!isascii(currentToken_))
+        //     return -1;
 
-    /// GetTokPrecedence - Get the precedence of the pending binary operator token.
-    static int GetTokPrecedence() {
-    if (!isascii(m_curTok))
-        return -1;
-
-    // Make sure it's a declared binop.
-    int TokPrec = m_binopPrecedence[m_curTok];
-    if (TokPrec <= 0)
-        return -1;
-    return TokPrec;
-    }
+        // // Make sure it's a declared binop.
+        // int TokPrec = m_binopPrecedence[currentToken_];
+        // if (TokPrec <= 0)
+        //     return -1;
+        // return TokPrec;
+        // }
+    private:
+        std::unique_ptr<Lexer> lexer_;
+        std::filesystem::path file_;
+        //int getNextToken() { return currentToken_ = gettok(); }
+        int currentToken_;
+        // // BinopPrecedence - This holds the precedence for each binary operator that is defined
+        // static std::map<char, int> m_binopPrecedence;
 };
