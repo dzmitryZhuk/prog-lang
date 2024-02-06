6929879fce1471b12ff4ff3ec8ed5a7d3465af88
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-06 16:42:21 +0300

    upd compiller with passing file to parser

--- src/compiller.cpp
+++ src/compiller.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <filesystem>
+#include "parser.h"
 
-int main()
+int main(int argc, char* argv[])
 {
   std::cout << "compiller" << std::endl;
-  return 0;
+
+  std::filesystem::path path = argv[1];
+
+  if(!std::filesystem::exists(path))
+  {
+    std::cerr << "File <" << path << "> don't exists!" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  if (!std::filesystem::is_regular_file(path)) {
+      std::cerr << "This path <" << path << "> is not a file!" << std::endl;
+      return EXIT_FAILURE;
+  }
+
+  // Parser parser{path};
+  // .
+  // .
+  // .
+
+  return EXIT_SUCCESS;
 }
