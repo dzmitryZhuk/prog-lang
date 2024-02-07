f3c364a54b5f7cb08689dfb6c3b055fcfdeb8453
dzmitryZhuk <dzmitry.zhuk@bk.ru>
2024-02-07 09:21:32 +0300

    add basic class ast node

--- src/ast.cpp
+++ src/ast.cpp
@@ -5,40 +5,50 @@ ASTExpressionNumber::ASTExpressionNumber(const double& value)
 {
 }
 
+const double ASTExpressionNumber::value() const
+{
+  return value_;
+}
+
 ASTExpressionVariable::ASTExpressionVariable(const std::string &name)
     : name_(name)
 {
 }
 
-ASTExpressionBinary::ASTExpressionBinary(const char &binaryOperator,
-                                         std::unique_ptr<ASTExpression> left,
-                                         std::unique_ptr<ASTExpression> right)
-    : operator_(binaryOperator)
-    , leftOperand_(std::move(left))
-    , rightOperand_(std::move(right))
+const std::string ASTExpressionVariable::name() const
 {
+  return name_;
 }
 
-ASTExpressionCall::ASTExpressionCall(const std::string &callee,
-                                     std::vector<std::unique_ptr<ASTExpression>> args)
-    : callee_(callee)
-    , args_(std::move(args))
-{
-}
+// ASTExpressionBinary::ASTExpressionBinary(const char &binaryOperator,
+//                                          std::unique_ptr<ASTExpression> left,
+//                                          std::unique_ptr<ASTExpression> right)
+//     : operator_(binaryOperator)
+//     , leftOperand_(std::move(left))
+//     , rightOperand_(std::move(right))
+// {
+// }
 
-ASTPrototype::ASTPrototype(const std::string &name, const std::vector<std::string> &args)
-    : name_(name)
-    , args_(std::move(args))
-{
-}
+// ASTExpressionCall::ASTExpressionCall(const std::string &callee,
+//                                      std::vector<std::unique_ptr<ASTExpression>> args)
+//     : callee_(callee)
+//     , args_(std::move(args))
+// {
+// }
 
-const std::string &ASTPrototype::name() const
-{
-    return name_;
-}
+// ASTPrototype::ASTPrototype(const std::string &name, const std::vector<std::string> &args)
+//     : name_(name)
+//     , args_(std::move(args))
+// {
+// }
 
-ASTFunction::ASTFunction(std::unique_ptr<ASTPrototype> proto, std::unique_ptr<ASTExpression> body)
-    : proto_(std::move(proto))
-    , body_(std::move(body))
-{
-}
+// const std::string &ASTPrototype::name() const
+// {
+//     return name_;
+// }
+
+// ASTFunction::ASTFunction(std::unique_ptr<ASTPrototype> proto, std::unique_ptr<ASTExpression> body)
+//     : proto_(std::move(proto))
+//     , body_(std::move(body))
+// {
+// }
--- src/ast.h
+++ src/ast.h
@@ -4,65 +4,76 @@
 #include <vector>
 #include <memory>
 
+class ASTNode
+{
+    // TODO: add type of node like number, string, etc.
+    public:
+        virtual ~ASTNode() = default;
+};
+
 // AST expression
-class ASTExpression {
-    // TODO: add type of expression like number, string, etc.
+class ASTExpression : public ASTNode
+{
     public:
         virtual ~ASTExpression() = default;
 };
 
 // Expression class for for numeric literals
-class ASTExpressionNumber : public ASTExpression {
+class ASTExpressionNumber : public ASTExpression
+{
     public:
         ASTExpressionNumber(const double &value);
+        const double value() const;
     private:
         double value_;
 };
 
 // Expression class for referencing a variable, like "val".
-class ASTExpressionVariable : public ASTExpression {
+class ASTExpressionVariable : public ASTExpression
+{
     public:
         ASTExpressionVariable(const std::string& name);
+        const std::string name() const;
     private:
         std::string name_;
 };
 
-// Expression class for a binary operator.
-class ASTExpressionBinary : public ASTExpression {
-    public:
-        ASTExpressionBinary(const char &binaryOperator,
-                          std::unique_ptr<ASTExpression> left,
-                          std::unique_ptr<ASTExpression> right);
-    private:
-        char operator_;
-        std::unique_ptr<ASTExpression> leftOperand_, rightOperand_;
-};
+// // Expression class for a binary operator.
+// class ASTExpressionBinary : public ASTExpression {
+//     public:
+//         ASTExpressionBinary(const char &binaryOperator,
+//                           std::unique_ptr<ASTExpression> left,
+//                           std::unique_ptr<ASTExpression> right);
+//     private:
+//         char operator_;
+//         std::unique_ptr<ASTExpression> leftOperand_, rightOperand_;
+// };
 
-// Expression class for function calls
-class ASTExpressionCall : public ASTExpression {
-    public:
-        ASTExpressionCall(const std::string &callee,
-                          std::vector<std::unique_ptr<ASTExpression>> args);
-    private:
-          std::string callee_;
-          std::vector<std::unique_ptr<ASTExpression>> args_;
-};
+// // Expression class for function calls
+// class ASTExpressionCall : public ASTExpression {
+//     public:
+//         ASTExpressionCall(const std::string &callee,
+//                           std::vector<std::unique_ptr<ASTExpression>> args);
+//     private:
+//           std::string callee_;
+//           std::vector<std::unique_ptr<ASTExpression>> args_;
+// };
 
-// This class represents the "prototype" for a function, which captures its name, and its argument names
-class ASTPrototype {
-    public:
-        ASTPrototype(const std::string &name, const std::vector<std::string> &args);
-        const std::string &name() const;
-    private:
-          std::string name_;
-          std::vector<std::string> args_;
-};
+// // This class represents the "prototype" for a function, which captures its name, and its argument names
+// class ASTPrototype {
+//     public:
+//         ASTPrototype(const std::string &name, const std::vector<std::string> &args);
+//         const std::string &name() const;
+//     private:
+//           std::string name_;
+//           std::vector<std::string> args_;
+// };
 
-// This class represents a function definition itself
-class ASTFunction {
-    public:
-        ASTFunction(std::unique_ptr<ASTPrototype> proto, std::unique_ptr<ASTExpression> body);
-    private:
-        std::unique_ptr<ASTPrototype> proto_;
-        std::unique_ptr<ASTExpression> body_;
-};
+// // This class represents a function definition itself
+// class ASTFunction {
+//     public:
+//         ASTFunction(std::unique_ptr<ASTPrototype> proto, std::unique_ptr<ASTExpression> body);
+//     private:
+//         std::unique_ptr<ASTPrototype> proto_;
+//         std::unique_ptr<ASTExpression> body_;
+// };
