9c1e68fac71eb3d2f94a35d859e42db5ae4749ac
Dzmitry Zhuk <dzmitry.zhuk@bk.ru>
2024-11-19 16:41:00 +0300

    fix reading source file in Parser constructor

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,11 +5,8 @@
 Parser::Parser(const std::filesystem::path &path)
     : file_(path)
 {
-  auto fileSize = std::filesystem::file_size(path);
-  std::string fileContent;
-  fileContent.reserve(fileSize);
   std::ifstream stream{path};
-  stream.read(fileContent.data(), fileSize);
+  std::string fileContent((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
   stream.close();
   lexer_ = std::make_unique<Lexer>(fileContent);
 }
