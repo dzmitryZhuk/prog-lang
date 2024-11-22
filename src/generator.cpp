eac0fe1bf010e11c57c615686f9f8a141b86ef5e
Dzmitry Zhuk <dzmitry.zhuk@bk.ru>
2024-11-22 18:01:08 +0300

    change determition of os and arch: replace #ifdef by switch case

--- src/generator.cpp
+++ src/generator.cpp
@@ -30,28 +30,39 @@ std::string Generator::generate()
 std::string Generator::generateHeader()
 {
   std::stringstream asmString;
-#ifdef __APPLE__
-  // #ifdef TARGET_OS_MAC
+  switch (cOs)
+  {
+  case OS::mac:
   asmString << std::endl;
   asmString << std::format(".global {}", nameOfEntry_) << std::endl;
   asmString << ".align 4" << std::endl;
   asmString << std::endl;
-  // #endif
-#endif 
+    break;
+  case OS::linux:
+  case OS::win:
+  default:
+    break;
+  }
   return asmString.str();
 }
 
 std::string Generator::generateFooter()
 {
   std::stringstream asmString;
-#ifdef __APPLE__
-  // #ifdef TARGET_OS_MAC
+  switch (cOs)
+  {
+  case OS::mac:
   asmString << std::endl;
   asmString << std::format("{}:", nameOfExit_) << std::endl;
   asmString << std::format("{}mov X0, #0", cIndent) << std::endl;   // Use 0 return code
   asmString << std::format("{}mov X16, #1", cIndent) << std::endl;  // #1 sys call - exit
-  asmString << std::format("{}svc	#0x80", cIndent) << std::endl;   // Call kernel
-  // #endif
-#endif 
+    asmString << std::format("{}svc	#0x80", cIndent) << std::endl;    // Call kernel
+    asmString << std::endl;
+    break;
+  case OS::linux:
+  case OS::win:
+  default:
+    break;
+  }
   return asmString.str();
 }
