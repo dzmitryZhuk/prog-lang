af974d08c5ee84d0720e8dc9786b3dbd5aa94153
Dzmitry Zhuk <dzmitry.zhuk@bk.ru>
2024-11-25 15:38:23 +0300

    add syscalls write and read to Generator

--- src/generator.cpp
+++ src/generator.cpp
@@ -66,3 +66,55 @@ std::string Generator::generateFooter()
   }
   return asmString.str();
 }
+
+
+std::string Generator::generateSysCallWrite(unsigned int fd, addr buf, addr count)
+{
+  std::stringstream asmString;
+  int syscall_number = 4; // arm (32-bit/EABI)
+#ifdef __APPLE__
+  // #ifdef TARGET_OS_MAC
+  asmString << std::endl;
+  asmString << std::format("{}mov X0, #{}", cIndent, fd) << std::endl;
+  asmString << std::format("{}mov X1, {}", cIndent, buf) << std::endl;
+  asmString << std::format("{}mov X2, {}", cIndent, count) << std::endl;
+  asmString << std::format("{}mov X16, #{}", cIndent, syscall_number) << std::endl;
+  asmString << std::format("{}svc	#0x80", cIndent) << std::endl;
+  asmString << std::endl;
+  // #endif
+#endif 
+  return asmString.str();
+}
+
+std::string Generator::generateSysCallRead(unsigned int fd, addr buf, addr count)
+{
+  std::stringstream asmString;
+  int syscall_number = 3; // arm (32-bit/EABI)
+#ifdef __APPLE__
+  // #ifdef TARGET_OS_MAC
+  asmString << std::endl;
+  asmString << std::format("{}mov X0, #{}", cIndent, fd) << std::endl;
+  asmString << std::format("{}mov X1, {}", cIndent, buf) << std::endl;
+  asmString << std::format("{}mov X2, {}", cIndent, count) << std::endl;
+  asmString << std::format("{}mov X16, #{}", cIndent, syscall_number) << std::endl;
+  asmString << std::format("{}svc	#0x80", cIndent) << std::endl;
+  asmString << std::endl;
+  // #endif
+#endif 
+  return asmString.str();
+}
+
+std::string Generator::generateSysCallReadStdin(addr buf, addr count)
+{
+    return generateSysCallRead(0, buf, count);
+}
+
+std::string Generator::generateSysCallWriteStdout(addr buf, addr count)
+{
+  return generateSysCallWrite(1, buf, count);
+}
+
+std::string Generator::generateSysCallWriteStderr(addr buf, addr count)
+{
+  return generateSysCallWrite(2, buf, count);
+}
--- src/generator.h
+++ src/generator.h
@@ -4,6 +4,8 @@
 
 #include <string>
 
+using addr = uint32_t;
+
 /*!
  * \brief Class that generates assembly code from AST
  */
@@ -17,6 +19,12 @@ class Generator
     protected:
         virtual std::string generateHeader();
         virtual std::string generateFooter();
+
+        virtual std::string generateSysCallWrite(unsigned int fd, addr buf, addr count);
+        virtual std::string generateSysCallRead(unsigned int fd, addr buf, addr count);
+        virtual std::string generateSysCallReadStdin(addr buf, addr count);
+        virtual std::string generateSysCallWriteStdout(addr buf, addr count);
+        virtual std::string generateSysCallWriteStderr(addr buf, addr count);
 
         /*!
         * \brief Name of label that respond for entry point of current object file
