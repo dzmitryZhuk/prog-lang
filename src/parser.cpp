9539d4b8087ee9fe9849b3a4588df6653a3f82b2
Dzmitry Zhuk <dzmitry.zhuk@bk.ru>
2024-11-27 15:16:53 +0300

    parser first parse implementation

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,6 +1,7 @@
 #include "parser.h"
 
 #include <fstream>
+#include <iostream>
 
 Parser::Parser(const std::filesystem::path &path)
     : file_(path)
@@ -9,4 +10,76 @@ Parser::Parser(const std::filesystem::path &path)
   std::string fileContent((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
   stream.close();
   lexer_ = std::make_unique<Lexer>(fileContent);
+}
+
+std::unique_ptr<ASTNode> Parser::parse()
+{
+  auto tokens = lexer_.get()->tokenize();
+  return parse(tokens);
+}
+
+std::unique_ptr<ASTNode> Parser::parse(const std::vector<Lexer::TokenTuple>& tokens)
+{
+  size_t currentIter = 0; // pointer for current token (iterator)
+  
+  auto currentToken = [&]() -> const Lexer::TokenTuple& {
+      if (currentIter >= tokens.size()) {
+          throw std::runtime_error("Unexpected end of input while parsing");
+      }
+      return tokens[currentIter];
+  };
+
+  auto checkToken = [&](Lexer::Token expectedType) -> bool {
+      return currentIter < tokens.size() && std::get<Lexer::Token>(currentToken()) == expectedType;
+  };
+
+  auto consumeToken = [&]() -> const Lexer::TokenTuple& {
+      const auto& token = currentToken();
+      ++currentIter;
+      return token;
+  };
+
+  // start parsing
+  std::unique_ptr<ASTNode> root = nullptr;
+
+  while (currentIter < tokens.size()) {
+      const auto& token = currentToken();
+
+      switch (std::get<Lexer::Token>(token)) {
+          case Lexer::Token::returning: {
+              // parse "ret <smth>;"
+              consumeToken(); // consume "ret"
+
+              // next token is number or variable
+              if (checkToken(Lexer::Token::number)) {
+                  double value = std::get<double>(std::get<Lexer::TokenValue>(consumeToken()));
+                  root = std::make_unique<ASTExpressionNumber>(value);
+              } else if (checkToken(Lexer::Token::identifier)) {
+                  std::string variableName = std::get<std::string>(std::get<Lexer::TokenValue>(consumeToken()));
+                  root = std::make_unique<ASTExpressionVariable>(variableName);
+              } else {  // TODO: add case if function call here
+                auto pos = std::get<Lexer::TokenPos>(consumeToken());
+                std::cout << std::format("line {}:{}:Expected a number or variable after 'ret'", pos.first, pos.second);
+                  // throw std::runtime_error("Expected a number or variable after 'ret'");
+              }
+
+              // make sure that construction ends with ";"
+              if (!checkToken(Lexer::Token::delimiter)) {
+                auto pos = std::get<Lexer::TokenPos>(consumeToken());
+                std::cout << std::format("line {}:{}:Expected '{}' after return statement", pos.first, pos.second, lexer_.get()->delimitertIdentifier());
+                  // throw std::runtime_error(std::format("Expected '{}' after return statement", lexer_.get()->delimitertIdentifier()));
+              }
+              consumeToken(); // consume ";"
+              break;
+          }
+          
+          default: {
+              // throw std::runtime_error("Unexpected token while parsing");
+              auto pos = std::get<Lexer::TokenPos>(consumeToken());
+              std::cout << std::format("line {}:{}:Unexpected token while parsing", pos.first, pos.second);
+          }
+      }
+  }
+
+  return root;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,35 +1,24 @@
 #pragma once
 
 #include "lexer.h"
+#include "ast.h"
 
 #include <map>
 #include <string>
 #include <filesystem>
+#include <memory>
 
 class Parser
 {
     public:
         Parser(const std::filesystem::path &path);
         virtual ~Parser() = default;
+        std::unique_ptr<ASTNode> parse();
 
-        // AST * parse();
+    protected:
+        std::unique_ptr<ASTNode> parse(const std::vector<Lexer::TokenTuple>& tokens);
 
-        // /// GetTokPrecedence - Get the precedence of the pending binary operator token.
-        // static int GetTokPrecedence() {
-        // if (!isascii(currentToken_))
-        //     return -1;
-
-        // // Make sure it's a declared binop.
-        // int TokPrec = m_binopPrecedence[currentToken_];
-        // if (TokPrec <= 0)
-        //     return -1;
-        // return TokPrec;
-        // }
     private:
         std::unique_ptr<Lexer> lexer_;
         std::filesystem::path file_;
-        //int getNextToken() { return currentToken_ = gettok(); }
-        int currentToken_;
-        // // BinopPrecedence - This holds the precedence for each binary operator that is defined
-        // static std::map<char, int> m_binopPrecedence;
 };
