17337a6776cd59e65677386e7a5425982126415c
Dzmitry Zhuk <dzmitry.zhuk@bk.ru>
2024-12-05 15:30:48 +0300

    upd main

--- src/compiller.cpp
+++ src/compiller.cpp
@@ -8,25 +8,26 @@ int main(int argc, char* argv[])
 {
   std::cout << "compiller" << std::endl;
 
-  std::filesystem::path path = argv[1];
+  std::filesystem::path input_path = argv[1];
 
-  if(!std::filesystem::exists(path))
+  if(!std::filesystem::exists(input_path))
   {
-    std::cerr << "File <" << path << "> don't exists!" << std::endl;
+    std::cerr << "File <" << input_path << "> don't exists!" << std::endl;
     return EXIT_FAILURE;
   }
 
-  if (!std::filesystem::is_regular_file(path)) {
-      std::cerr << "This path <" << path << "> is not a file!" << std::endl;
+  if (!std::filesystem::is_regular_file(input_path)) {
+      std::cerr << "This path <" << input_path << "> is not a file!" << std::endl;
       return EXIT_FAILURE;
   }
   
-  Parser parser{path};
+  Parser parser{input_path};
   auto ast = parser.parse();
   Generator gen(std::move(ast));
   auto generated_str = gen.generate();
   
-  std::fstream f("out.txt", std::ios::out);
+  std::filesystem::path output_path = input_path.parent_path().append("out.asm");
+  std::fstream f(output_path, std::ios::out);
   f << generated_str;
   f.close();
 
